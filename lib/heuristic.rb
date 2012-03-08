2f79a22b5dbc3919208f55361adf9a367ea5b275
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-08 09:39:02 -0600

    Getting rid of tabs

--- lib/heuristic.rb
+++ lib/heuristic.rb
@@ -1,8 +1,9 @@
 class Heuristic
-	@@WINNING_ROWS = [  [1, 2, 3], [4, 5, 6], [7, 8, 9],
-											[1, 4, 7], [2, 5, 8], [3, 6, 9],
+  @@WINNING_ROWS = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
+                    [1, 4, 7], [2, 5, 8], [3, 6, 9],
 											[1, 5, 9], [3, 5, 7] ]
-											
+ 
+ 
   def initialize(board)
     @board = board
   end
@@ -13,18 +14,18 @@ class Heuristic
   def getWinningMove(player)
     result = nil
 
-		@@WINNING_ROWS.each do |row|
-    	squaresOwnedByPlayer = getSquaresOwnedByPlayer(row, player)
-    	emptySquares = getEmptySquares(row)
+    @@WINNING_ROWS.each do |row|
+      squaresOwnedByPlayer = getSquaresOwnedByPlayer(row, player)
+      emptySquares = getEmptySquares(row)
 
-    	if squaresOwnedByPlayer.count == 2
-      	if emptySquares.count == 1
-      	  result ||= emptySquares[0]
-      	end
-    	end
-		end
-		result
-	end
+      if squaresOwnedByPlayer.count == 2
+        if emptySquares.count == 1
+          result ||= emptySquares[0]
+        end
+      end
+    end
+    result
+  end
   def getSquaresOwnedByPlayer(squares, player)
     result = Array.new
     squares.each do |square|
