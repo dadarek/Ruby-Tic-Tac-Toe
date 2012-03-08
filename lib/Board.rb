6b60a626db0f046a4f68e8348377decdd3fd11f9
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-08 11:38:34 -0600

    Slowly moving board functionality into board (and out of the Heuristic)

--- lib/Board.rb
+++ lib/Board.rb
@@ -5,12 +5,20 @@ class Board
   def takeSquare(square, player)
     @squares[square] = player
   end
-
+  def take(square, player)
+    takeSquare(square, player)
+  end
   def isEmpty(square)
     @squares[square].nil?
   end
   def playerOwnsSquare(player, square)
     @squares[square] == player
+  end
+  def getPlayerSquares(player)
+    @squares.collect{ |key, value| key if value == player }.compact()
+  end
+  def getEmptySquares()
+    (1..9).select{ |i| @squares[i].nil? }
   end
 end
 
--- spec/Board_spec.rb
+++ spec/Board_spec.rb
@@ -1,19 +1,38 @@
 require 'board'
 
 describe Board, "#playerAt" do
-  it "has empty squares" do
-    board = Board.new
+  before(:each) do
+    @board = Board.new
+  end
 
-    (1..10).each{|i| board.isEmpty(i).should == true }
+  it "has empty squares" do
+    (1..9).each{|i| @board.isEmpty(i).should == true }
   end
   it "remembers players squares" do
-    board = Board.new
+    @board.take(1, "x")
+    @board.take(3, "x")
 
-    board.takeSquare(1, "x")
-    board.takeSquare(2, "o")
+    @board.getPlayerSquares("x").should == [1, 3]
+  end
+  it "remembers 2 players squares" do
+    @board.take(2, "x")
+    @board.take(5, "x")
+    @board.take(9, "x")
+    
+    @board.take(8, "o")
+    @board.take(1, "o")
 
-    board.playerOwnsSquare("x", 1).should == true
-    board.playerOwnsSquare("o", 2).should == true
-    board.playerOwnsSquare("x", 3).should == false
+    @board.getPlayerSquares("x").sort.should == [2, 5, 9]
+    @board.getPlayerSquares("o").sort.should == [1, 8]
+  end
+
+  it "knows its empty squares" do
+    @board.getEmptySquares().should == (1..9).to_a()
+  end
+  it "removes empty squares after players take them" do
+    @board.take(5, "b")
+    @board.take(9, "c")
+
+    @board.getEmptySquares().should == [1, 2, 3, 4, 6, 7, 8]
   end
 end 
