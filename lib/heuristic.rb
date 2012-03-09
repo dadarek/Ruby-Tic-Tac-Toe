f46bb3d273a496093b20489243e7feae18dc7dfa
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-09 10:53:53 -0600

    Looking good :)

--- lib/heuristic.rb
+++ lib/heuristic.rb
@@ -8,22 +8,49 @@ class Heuristic
     @player = player
     @opponent = opponent
   end
+
   def nextMove()
     result ||= findWinningMoveFor(@player)
     result ||= findWinningMoveFor(@opponent)
     result ||= tryFork()
+    result ||= blockFork()
   end
-  def tryFork()
-    3
-  end
+
   def findWinningMoveFor(somePlayer)
     possibleWin = @@WINNING_ROWS.detect { |row| canWin(row, somePlayer) }
     (possibleWin & @board.getEmptySquares())[0] unless possibleWin.nil?
   end
+
   def canWin(row, player)
     squaresOwnedByPlayer = row & @board.getPlayerSquares(player)
     emptySquares = row & @board.getEmptySquares()
 
     squaresOwnedByPlayer.count == 2 and emptySquares.count == 1
   end
+
+  def tryFork()
+    possibleWins = @@WINNING_ROWS.select{ |row| playerOwnsRow(row) }
+    commonSquares = getCommonSquares(possibleWins)
+    commonSquares.detect{ |square| @board.isEmpty(square) }
+  end
+
+  def playerOwnsRow(row)
+    opponentSquares = row & @board.getPlayerSquares(@opponent)
+    playerSquares = row & @board.getPlayerSquares(@player)
+    opponentSquares.count == 0 and playerSquares.count > 0
+  end
+
+  def getCommonSquares(rows)
+    occurences = { }
+    (1..9).each{ |i| occurences[i] = 0 }
+    rows.each{ |row| row.each{ |square| occurences[square] += 1 } }
+    
+    occurences.delete_if{ |square, count| count < 2 }
+    occurences.keys
+  end
+
+  def blockFork()
+    2
+  end
+
 end
--- spec/heuristic_spec.rb
+++ spec/heuristic_spec.rb
@@ -12,6 +12,7 @@ describe Heuristic do
     @board.take(2, "x")
     @heuristic.nextMove().should == 3
   end
+
   it "takes the winning move (diagonal)" do
     @board.take(1, "x")
     @board.take(9, "x")
@@ -23,18 +24,36 @@ describe Heuristic do
     @board.take(5, "o")
     @heuristic.nextMove().should == 6
   end
+
   it "blocks opponents winning move (diagonal)" do
     @board.take(3, "o")
     @board.take(7, "o")
     @heuristic.nextMove().should == 5
   end
 
-  it "forks when possible" do
+  it "forks when possible (case 1)" do
     @board.take(1, "x")
     @board.take(9, "o")
     @board.take(7, "x")
     @board.take(4, "o")
 
     @heuristic.nextMove().should == 3
+  end
+
+  it "forks when possible (case 2)" do
+    @board.take(1, "x")
+    @board.take(7, "o")
+    @board.take(3, "x")
+    @board.take(2, "o")
+
+    @heuristic.nextMove().should == 9
+  end
+
+  it "blocks fork" do
+    @board.take(1, "o")
+    @board.take(5, "x")
+    @board.take(9, "o")
+
+    @heuristic.nextMove().should satisfy{ |square| [2, 4, 6, 8].include? square }
   end
 end
