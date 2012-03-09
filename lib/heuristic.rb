e95f3d39a434626513618cc59b410481df9abf93
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-09 10:03:56 -0600

    Moving back to original huristic.  Some cleanup

--- lib/heuristic.rb
+++ lib/heuristic.rb
@@ -3,35 +3,27 @@ class Heuristic
                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
                     [1, 5, 9], [3, 5, 7] ]
  
-  def initialize(board)
+  def initialize(board, player, opponent)
     @board = board
+    @player = player
+    @opponent = opponent
   end
-  def nextMove(player)
-    result ||= getWinningMove(player)
-    result ||= getBlockingMove(player)
-    result ||= tryFork(player)
+  def nextMove()
+    result ||= findWinningMoveFor(@player)
+    result ||= findWinningMoveFor(@opponent)
+    result ||= tryFork()
   end
-  def tryFork(player)
+  def tryFork()
     3
   end
-  def getWinningMove(player)
-    possibleWin = @@WINNING_ROWS.detect { |row| canWin(row, player) }
+  def findWinningMoveFor(somePlayer)
+    possibleWin = @@WINNING_ROWS.detect { |row| canWin(row, somePlayer) }
     (possibleWin & @board.getEmptySquares())[0] unless possibleWin.nil?
-  end
-  def getBlockingMove(player)
-    possibleLoss = @@WINNING_ROWS.detect { |row| canLose(row, player) } 
-    (possibleLoss & @board.getEmptySquares())[0] unless possibleLoss.nil?
   end
   def canWin(row, player)
     squaresOwnedByPlayer = row & @board.getPlayerSquares(player)
     emptySquares = row & @board.getEmptySquares()
 
     squaresOwnedByPlayer.count == 2 and emptySquares.count == 1
-  end
-  def canLose(row, player)
-    squaresOwnedByOpponent = row & @board.getOpponentSquares(player)
-    emptySquares = row & @board.getEmptySquares()
-    
-    squaresOwnedByOpponent.count == 2 and emptySquares.count == 1
   end
 end
--- spec/heuristic_spec.rb
+++ spec/heuristic_spec.rb
@@ -4,29 +4,29 @@ require 'board'
 describe Heuristic do
   before(:each) do
     @board = Board.new
-    @heuristic = Heuristic.new(@board)
+    @heuristic = Heuristic.new(@board, "x", "o")
   end
 
   it "takes the winning move" do
     @board.take(1, "x")
     @board.take(2, "x")
-    @heuristic.nextMove("x").should == 3
+    @heuristic.nextMove().should == 3
   end
   it "takes the winning move (diagonal)" do
     @board.take(1, "x")
     @board.take(9, "x")
-    @heuristic.nextMove("x").should == 5
+    @heuristic.nextMove().should == 5
   end
 
   it "blocks opponents winning move" do
-    @board.take(4, "x")
-    @board.take(5, "x")
-    @heuristic.nextMove("o").should == 6
+    @board.take(4, "o")
+    @board.take(5, "o")
+    @heuristic.nextMove().should == 6
   end
   it "blocks opponents winning move (diagonal)" do
-    @board.take(3, "x")
-    @board.take(7, "x")
-    @heuristic.nextMove("o").should == 5
+    @board.take(3, "o")
+    @board.take(7, "o")
+    @heuristic.nextMove().should == 5
   end
 
   it "forks when possible" do
@@ -35,6 +35,6 @@ describe Heuristic do
     @board.take(7, "x")
     @board.take(4, "o")
 
-    @heuristic.nextMove("x").should == 3
+    @heuristic.nextMove().should == 3
   end
 end
