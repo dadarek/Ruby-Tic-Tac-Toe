17e4be0dff43577179d69df2d94e5e940313e93d
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-12 13:32:26 -0500

    refactoring heuristic specs

--- spec/heuristic_spec.rb
+++ spec/heuristic_spec.rb
@@ -8,130 +8,74 @@ describe Heuristic do
   end
 
   it "takes the winning move" do
-    @board.take(1, "x")
-    @board.take(2, "x")
-    @heuristic.nextMove().should == 3
+    take_x_o_and_assert([1, 2], [], 3)
   end
 
   it "takes the winning move (diagonal)" do
-    @board.take(1, "x")
-    @board.take(9, "x")
-    @heuristic.nextMove().should == 5
+    take_x_o_and_assert([1, 9], [], 5)
   end
 
   it "blocks opponents winning move" do
-    @board.take(4, "o")
-    @board.take(5, "o")
-    @heuristic.nextMove().should == 6
+    take_x_o_and_assert([], [4, 5], 6)
   end
 
   it "blocks opponents winning move (diagonal)" do
-    @board.take(3, "o")
-    @board.take(7, "o")
-    @heuristic.nextMove().should == 5
+    take_x_o_and_assert([], [3, 7], 5)
   end
 
   it "forks when possible (case 1)" do
-    @board.take(1, "x")
-    @board.take(9, "o")
-    @board.take(7, "x")
-    @board.take(4, "o")
-
-    @heuristic.nextMove().should == 3
+    take_x_o_and_assert([1, 7], [9, 4], 3)
   end
 
   it "forks when possible (case 2)" do
-    @board.take(1, "x")
-    @board.take(7, "o")
-    @board.take(3, "x")
-    @board.take(2, "o")
-
-    @heuristic.nextMove().should == 9
+    take_x_o_and_assert([1, 3], [7, 2], 9)
   end
 
   it "blocks possible fork on second move" do
-    @board.take(1, "o")
+    take([1, 9], "o")
     @board.take(5, "x")
-    @board.take(9, "o")
 
     @heuristic.nextMove().should satisfy{ |square| [2, 4, 6, 8].include? square }
   end
 
   it "blocks fork when opponents first move is edge" do
-    @board.take(8, "o")
+    take([8, 1], "o")
     @board.take(5, "x")
-    @board.take(1, "o")
     @heuristic.nextMove().should satisfy{ |square| [4, 7, 9].include? square }
   end
 
   it "takes the center if open" do
-    @board.take(9, "x")
-    @board.take(4, "o")
-    @heuristic.nextMove().should == 5
+    take_x_o_and_assert([9], [4], 5)
   end
 
   it "makes the correct first move (any corner)" do
     @heuristic.nextMove().should satisfy{ |square| [1,3,7,9].include? square }
   end
 
-  it "makes a move when the game is almost over" do
-    @board.take(1, "x")
-    @board.take(5, "o")
-    @board.take(9, "x")
-    @board.take(4, "o")
-    @board.take(6, "x")
-    @board.take(3, "o")
-    @board.take(7, "x")
-    @board.take(8, "o")
-    @heuristic.nextMove().should == 2
+  it "takes the last square (2)" do
+    take_x_o_and_assert([3, 4, 8, 9], [1, 5, 6, 7], 2)
   end
 
-  it "takes the last square" do
-    @board.take(3, "x")
-    @board.take(4, "x")
-    @board.take(8, "x")
-    @board.take(9, "x")
-    @board.take(1, "o")
-    @board.take(5, "o")
-    @board.take(6, "o")
-    @board.take(7, "o")
-    @heuristic.nextMove().should == 2
+  it "takes the last square (8)" do
+    take_x_o_and_assert([1, 2, 6, 7], [3, 4, 5, 9], 8)
   end
 
-  it "takes the last square" do
-    @board.take(1, "x")
-    @board.take(2, "x")
-    @board.take(6, "x")
-    @board.take(7, "x")
-    @board.take(3, "o")
-    @board.take(4, "o")
-    @board.take(5, "o")
-    @board.take(9, "o")
-    @heuristic.nextMove().should == 8
+  it "takes the last square (4)" do
+    take_x_o_and_assert([1, 3, 6, 8], [2, 5, 7, 9], 4)
   end
 
-  it "takes the last square" do
-    @board.take(1, "x")
-    @board.take(3, "x")
-    @board.take(6, "x")
-    @board.take(8, "x")
-    @board.take(2, "o")
-    @board.take(5, "o")
-    @board.take(7, "o")
-    @board.take(9, "o")
-    @heuristic.nextMove().should == 4
+  it "takes the last square (6)" do
+    take_x_o_and_assert([2, 4, 7, 9], [1, 3, 5, 8], 6)
   end
 
-  it "takes the last square" do
-    @board.take(2, "x")
-    @board.take(4, "x")
-    @board.take(7, "x")
-    @board.take(9, "x")
-    @board.take(1, "o")
-    @board.take(3, "o")
-    @board.take(5, "o")
-    @board.take(8, "o")
-    @heuristic.nextMove().should == 6
+  def take_x_o_and_assert(x_squares, o_squares, expectedMove)
+    take(x_squares, "x")
+    take(o_squares, "o")
+    @heuristic.nextMove.should == expectedMove
+  end
+
+  def take(squares, player)
+    squares.each{ |square| @board.take(square, player) }
   end
 
 end
