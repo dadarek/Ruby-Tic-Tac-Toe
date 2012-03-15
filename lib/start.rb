20942aa2cdc31928f816df61c898879b263e13b8
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-15 14:22:44 -0500

    Adding very tiny optimization to speed up calculation of first move.

--- lib/start.rb
+++ lib/start.rb
@@ -1,6 +1,7 @@
 require_relative 'game'
 require_relative 'board'
 require_relative 'heuristic'
+require_relative 'minimax_heuristic'
 require_relative 'computer_player'
 require_relative 'human_player'
 require_relative 'winner_finder'
@@ -13,7 +14,7 @@ ui = StreamUI.new
 prompter = UserPrompter.new
 
 p1 = HumanPlayer.new prompter
-p2 = ComputerPlayer.new Heuristic.new
+p2 = ComputerPlayer.new MinimaxHeuristic.new
 
 factory = GameFactory.new(Game, Board, p1, p2, ui)
 
