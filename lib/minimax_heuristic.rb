2c04003fd81b8d8e9f8234b5cd209f83510425af
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-15 10:26:38 -0500

    Different approach at minimax - this is looking promising

--- lib/minimax_heuristic.rb
+++ lib/minimax_heuristic.rb
@@ -1,5 +1,8 @@
 class MinimaxHeuristic
-  def possible_wins(board, player, square)
-    square == 8 ? 0 : 1
+# @@POSSIBLE_WINS = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
+#                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
+#                     [1, 5, 9], [3, 5, 7] ]
+  def score(player_squares, opponent_squares)
+    [1, 4, 7] & player_squares == [1, 4, 7] ? 1 : 0
   end
 end
--- spec/minimax_heuristic_spec.rb
+++ spec/minimax_heuristic_spec.rb
@@ -2,22 +2,13 @@ require 'minimax_heuristic'
 require 'board_utilities'
 
 describe MinimaxHeuristic do
-  before(:each) do
-    @heuristic = MinimaxHeuristic.new
-    @board = Board.new("x", "o")
+  it "scores tie as 0" do
+    heuristic = MinimaxHeuristic.new
+    heuristic.score([1, 3, 4, 6, 8], [2, 5, 7, 9]).should == 0
   end
 
-  it "knows there are zero ways of winning on last tie move" do
-    take_x_o(@board, [1, 3, 4, 6], [2, 5, 7, 9])
-    assert_wins_x(8, 0)
-  end
-
-  it "knows there is one way of winning on last winning move" do
-    take_x_o(@board, [1, 2, 6, 7], [4, 5, 8, 9])
-    assert_wins_x(3, 1)
-  end
-
-  def assert_wins_x(square, expected_wins)
-    @heuristic.possible_wins(@board, "x", square).should == expected_wins
+  it "scores win as 1" do
+    heuristic = MinimaxHeuristic.new
+    heuristic.score([1, 3, 4, 6, 7], [2, 5, 8, 9]).should == 1
   end
 end
