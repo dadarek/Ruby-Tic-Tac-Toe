471b2e69af02a93b1ccf2f22f95c56fdcc1c99dd
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-15 10:51:50 -0500

    Refactoring

--- lib/minimax_heuristic.rb
+++ lib/minimax_heuristic.rb
@@ -1,8 +1,19 @@
+require 'set'
+
 class MinimaxHeuristic
 # @@POSSIBLE_WINS = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
 #                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
 #                     [1, 5, 9], [3, 5, 7] ]
-  def score(player_squares, opponent_squares)
-    [1, 4, 7] & player_squares == [1, 4, 7] ? 1 : 0
+  def self.score(player_squares, opponent_squares)
+    if contains?([1, 4, 7], player_squares)
+      return 1
+    elsif contains?([1, 4, 7], opponent_squares)
+      return -1
+    end
+    0
+  end
+  
+  def self.contains?(winning_squares, player_squares)
+    winning_squares.to_set.subset? player_squares.to_set
   end
 end
--- spec/minimax_heuristic_spec.rb
+++ spec/minimax_heuristic_spec.rb
@@ -3,12 +3,18 @@ require 'board_utilities'
 
 describe MinimaxHeuristic do
   it "scores tie as 0" do
-    heuristic = MinimaxHeuristic.new
-    heuristic.score([1, 3, 4, 6, 8], [2, 5, 7, 9]).should == 0
+    assert_score([1, 3, 4, 6, 8], [2, 5, 7, 9], 0)
   end
 
   it "scores win as 1" do
-    heuristic = MinimaxHeuristic.new
-    heuristic.score([1, 3, 4, 6, 7], [2, 5, 8, 9]).should == 1
+    assert_score([1, 3, 4, 6, 7], [2, 5, 8, 9], 1)
+  end
+
+  it "scores loss as -1" do
+    assert_score([2, 5, 8, 9], [1, 3, 4, 6, 7], -1)
+  end
+
+  def assert_score(p1_squares, p2_squares, expected_score)
+    MinimaxHeuristic.score(p1_squares, p2_squares).should == expected_score
   end
 end
