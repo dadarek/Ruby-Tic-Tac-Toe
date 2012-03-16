e97541515860b780716e64ed7725c792b5e10bc4
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-16 15:43:25 -0500

    Cleanup

--- spec/dummies/dummy_in.rb
+++ spec/dummies/dummy_in.rb
@@ -1,21 +1,8 @@
 class DummyIn
-  attr_writer :read_square_responses, :play_again_responses, :play_first_responses
   attr_accessor :buffer
 
   def initialize
     buffer = ""
-  end
-
-  def read_square
-    @read_square_responses.shift
-  end
-
-  def play_again?
-    @play_again_responses.shift
-  end
-
-  def play_first?
-    @play_first_responses.shift
   end
 
   def gets
--- spec/dummies/dummy_out.rb
+++ spec/dummies/dummy_out.rb
@@ -1,24 +1,8 @@
 class DummyOut
-  attr_reader :times_prompted_for_square, :times_prompted_to_play_again, :times_prompted_to_play_first
   attr_accessor :buffer
 
   def initialize
-    @times_prompted_for_square = 0
-    @times_prompted_to_play_again = 0
-    @times_prompted_to_play_first = 0
     @buffer = ""
-  end
-
-  def prompt_for_square
-    @times_prompted_for_square += 1
-  end
-
-  def prompt_to_play_again
-    @times_prompted_to_play_again += 1
-  end
-
-  def prompt_to_play_first
-    @times_prompted_to_play_first += 1
   end
 
   def printf(s)
