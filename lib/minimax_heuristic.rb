9b7fbc6ef704ba68737555e8e376b45e780fcfeb
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-19 18:25:33 -0500

    Woo-Hoo!! Got it! Refactoring COMPLETE!

--- lib/minimax_heuristic.rb
+++ lib/minimax_heuristic.rb
@@ -5,13 +5,13 @@ class MinimaxHeuristic
   def next_move(board, player)
     opponent = player == board.p1 ? board.p2 : board.p1
     opponent_squares = board.get_player_squares opponent
-    
     player_squares = board.get_player_squares player
+
+    helper = MinimaxHeuristicHelper.new(player_squares, opponent_squares)
 
     empty_squares = board.get_empty_squares
 
     result ||= 1 if empty_squares.count == 9
-    result ||= empty_squares.detect{ |square| 1 == MinimaxHeuristicHelper.score(player_squares + Array(square), opponent_squares) }
-    result ||= empty_squares.max_by{ |square| MinimaxHeuristicHelper.score_if_takes_square(player_squares, opponent_squares, square) }
+    result ||= empty_squares.max_by{ |square| helper.value? square }
   end
 end
--- lib/minimax_heuristic_helper.rb
+++ lib/minimax_heuristic_helper.rb
@@ -8,15 +8,6 @@ class MinimaxHeuristicHelper
     @opponent = opponent
   end
   
-  def self.score(player, opponent)
-    if won? player
-      return 1
-    elsif won? opponent
-      return -1
-    end
-    0
-  end
-
   def value?(next_square)
     if win? next_square
       return 1
@@ -43,22 +34,6 @@ class MinimaxHeuristicHelper
 
   def tie?
     8 == @player.count + @opponent.count
-  end
-
-  def self.score_if_takes_square(player, opponent, next_square)
-    new_player = player + Array(next_square)
-
-    result = score(new_player, opponent)
-    if result == 0 and not last_move?(new_player, opponent)
-      open_squares = Array(1..9) - new_player - opponent
-      possible_opponent_results = open_squares.collect{ |square| score_if_takes_square(opponent, new_player, square) }
-      result = (possible_opponent_results.max) * -1
-    end
-    result
-  end
-
-  def self.last_move?(player, opponent)
-    player.count + opponent.count == 9
   end
 
   def self.won?(squares)
